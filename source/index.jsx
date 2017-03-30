5c10859b5ab16ab21aef60a1e943efa6aca7be64
Simon Lu <lusimon95@gmail.com>
2017-03-30 13:52:02 -0500

    switched to HashRouter to fix server side routing error

--- source/index.jsx
+++ source/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
 // Components
 import Home from './components/home/home';
