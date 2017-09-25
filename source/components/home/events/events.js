447fb73336322ce5221f06f3d65199f82db1da03
Aashish Kapur <aashish97@gmail.com>
2017-09-25 09:44:44 -0500

    schedule

--- source/components/home/events/events.js
+++ source/components/home/events/events.js
@@ -13,10 +13,9 @@ class Events extends Component {
         return(
             <div id="Events" className="Events container">
                 <PageHeader className="Events__header">Schedule</PageHeader>
-                <Alert bsStyle="info" onDismiss={this.handleAlertDismiss}>
-                  <h4>More details to be announced!</h4>
-                    <p>Check back later for scheduling details. <br />In the meantime, make sure you're <a href="#">registered for the conference!</a></p>
-                </Alert>
+                {/* <Alert bsStyle="info" onDismiss={this.handleAlertDismiss}> */}
+                    <h3><Button href="http://reflectionsprojections.org/schedule" className="Events__button caps">View our schedule</Button></h3>
+                {/* </Alert> */}
             </div>
         )
     }
